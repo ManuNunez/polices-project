d51a1d78378041192a97d46a21465f888326924e
ManuNunez <manu.nunez.2004@gmail.com>
2023-04-24 10:58:01 -0600

    struct update, main addpolice function added

--- main.c
+++ main.c
@@ -10,13 +10,13 @@
 */
 struct addres
 {
-    char street [20];
-    char suburb [20];
-    char city [20];
-    char state [20];
-    char country [3];
-    char streetNumber [5];
-    char innerNumber[5]; 
+    char street[20];
+    char suburb[20];
+    char city[20];
+    char state[20];
+    char country[3];
+    char streetNumber[5];
+    char innerNumber[5];
 };
 struct person
 {
@@ -38,7 +38,7 @@ struct date
 struct policyGeneral
 {
     float deductible;
-    float coverage;
+    char coverage[15];
     float price;
     float sumAssured;
     struct date vigency;
@@ -50,8 +50,7 @@ struct policyOwner
     char lastname[15];
     char contactPhone[10];
     char contactMail[20];
-    struct addres addres;   
-    int age;
+    struct addres addres;
 };
 
 struct policyCar
@@ -60,13 +59,11 @@ struct policyCar
     char brand[10];
     int serialNumber;
     int year;
-    
 };
 
 struct policyBuissnes
 {
     struct addres addresInsured;
-    
 };
 struct policyHome
 {
@@ -84,7 +81,7 @@ union policyType
     struct policyBuissnes Buissnes;
     struct policyHome Home;
     struct policyCar Car;
-    struct policyLife Home;
+    struct policyLife Life;
 };
 
 struct policy
@@ -97,12 +94,66 @@ struct policy
 /*
     polices functions
 */
-void addPolicy(struct policy p[], int *id)
+void addPolicy(struct policy p[], int *id, int policyType)
 {
-    p[*id].genaralData.coverage;
-    p[*id].genaralData.vigency;
-    p[*id].genaralData.price = 2332;
-    p[*id].genaralData.sumAssured = 33;
+    /*
+        float deductible;
+    float coverage;
+    float price;
+    float sumAssured;
+    struct date vigency;
+    */
+    printf("---AGREGAR POLIZA---");
+    printf("introduce el nombre del dueño de la poliza");
+    scanf("%c", &p[*id].policyOwner.name);
+    printf("introduce el apellido del dueño de la poliza");
+    scanf("%c", &p[*id].policyOwner.lastname);
+    printf("introduce el numero de telefono del dueño de la poliza");
+    scanf("%c", &p[*id].policyOwner.contactPhone);
+    printf("introduce el correo electronico del dueño de la poliza");
+    scanf("%c", &p[*id].policyOwner.contactMail);
+    printf("introduce la dirección del dueño");
+    printf("pais");
+    scanf("%c", &p[*id].policyOwner.addres.country);
+    printf("estado");
+    scanf("%c", &p[*id].policyOwner.addres.state);
+    printf("ciudad");
+    scanf("%c", &p[*id].policyOwner.addres.city);
+    printf("colonia");
+    scanf("%c", &p[*id].policyOwner.addres.suburb);
+    printf("calle");
+    scanf("%c", &p[*id].policyOwner.addres.street);
+    printf("numero exterior");
+    scanf("%c", &p[*id].policyOwner.addres.streetNumber);
+    printf("numero interior");
+    scanf("%c", &p[*id].policyOwner.addres.innerNumber);
+    printf("introduce el precio de la poliza");
+    scanf("%f", &p[*id].genaralData.price);
+    printf("introduce el deducible de la poliza");
+    scanf("%f", &p[*id].genaralData.deductible);
+    printf("introduce la suma asegurada de la poliza");
+    scanf("%f", &p[*id].genaralData.sumAssured);
+    printf("introduce el tipo de covertura de la poliza");
+    scanf("%c", &p[*id].genaralData.coverage);
+    printf("introduce la fecha de vigencia de la poliza");
+    printf("año");
+    scanf("%c", &p[*id].genaralData.vigency.year);
+     printf("mes");
+    scanf("%c", &p[*id].genaralData.vigency.month);
+     printf("dia");
+    scanf("%c", &p[*id].genaralData.vigency.day);
+
+    switch (policyType)
+    {
+        
+    case 1:
+
+        break;
+
+    default:
+        break;
+    }
+
     (*id)++;
 }
 void removePolicy()
@@ -117,8 +168,30 @@ void showPolicy()
 /*
     menus functions
 */
-void selectPolicyType()
+int selectPolicyType()
 {
+    int optionSelected;
+    int bool;
+    while (bool = 0)
+    {
+
+        printf("seleccione el tipo de poliza que desea agregar \n");
+        printf("1- Negocio \n");
+        printf("2- Hogar \n");
+        printf("3- Auto \n");
+        printf("4- Vida \n");
+        print("5- SALIR \n");
+        scanf("%d", &optionSelected);
+        if (optionSelected >= 0 && optionSelected <= 5)
+        {
+            bool ++;
+            return optionSelected;
+        }
+        else
+        {
+            printf("La opcion seleccionada es incorrecta");
+        }
+    }
 }
 void mainMenu(struct poliza *po, int *it)
 {
@@ -131,7 +204,12 @@ void mainMenu(struct poliza *po, int *it)
         switch (opcion)
         {
         case 1:
-            addPolicy(po, &it);
+            int policyType;
+            policyType = selectPolicyType();
+            if (policyType <= 4 && policyType >= 1)
+            {
+                addPolicy(po, &it, policyType);
+            }
         case 2: // eliminar
         case 3: // Buscar
         case 4: // editar
@@ -150,4 +228,5 @@ int main()
     int i = 0;
     mainMenu(ls, &i);
     // p.t.c.anio=2021;
+    return 0;
 }
--- policesStruct.h
+++ policesStruct.h
@@ -1,48 +1,86 @@
 #ifndef POLICESFUNCTIONS_H
 #define POLICESSTRUCT_H
 
-typedef struct policyGeneral
+struct addres
 {
-    /* data */
+    char street[20];
+    char suburb[20];
+    char city[20];
+    char state[20];
+    char country[3];
+    char streetNumber[5];
+    char innerNumber[5];
+};
+struct person
+{
+    char name[15];
+    char lastname[15];
+    struct date birth;
+    struct addres addres;
+    int age;
+    char contactPhone[10];
+    char contactMail[20];
+};
+struct date
+{
+    int day;
+    char month[3];
+    int year[];
 };
 
-
-typedef struct policyOwner
+struct policyGeneral
 {
-    char name [15];
-    char lastname [15];
-    char contactPhone [10];
-    
+    float deductible;
+    char coverage[15];
+    float price;
+    float sumAssured;
+    struct date vigency;
 };
 
-typedef struct policyCar
+struct policyOwner
 {
-    /* data */
+    char name[15];
+    char lastname[15];
+    char contactPhone[10];
+    char contactMail[20];
+    struct addres addres;
 };
 
-typedef struct policyBuissnes
+struct policyCar
 {
-    /* data */
+    char model[10];
+    char brand[10];
+    int serialNumber;
+    int year;
 };
 
-typedef struct policyHome
+struct policyBuissnes
 {
-    /* data */
+    struct addres addresInsured;
+};
+struct policyHome
+{
+    struct addres addresInsured;
 };
 
-typedef struct policyLife
+struct policyLife
 {
-    /* data */
+    struct person beneficiary;
+    struct person insured;
 };
 
-typedef union policyType
+union policyType
 {
-    /* data */
+    struct policyBuissnes Buissnes;
+    struct policyHome Home;
+    struct policyCar Car;
+    struct policyLife Life;
 };
 
-typedef struct policy
+struct policy
 {
-    /* data */
+    union policyType type;
+    struct policyGeneral genaralData;
+    struct policyOwner policyOwner;
 };
-
 #endif // POLICES_H
